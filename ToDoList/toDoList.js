9a97b9c3321ecb6e0d1aa5f6c84a4e6e19d77cae
sergkarapetian <sergey.karapetyan.1997@mail.ru>
2022-09-02 23:17:36 +0400

    uploaded new folder

---
+++ ToDoList/toDoList.js
@@ -0,0 +1,65 @@
+let header = document.getElementById("header");
+
+let input = document.createElement("input");
+input.classList.add("input");
+header.append(input);
+
+let reset = document.createElement("input");
+reset.setAttribute("type", "reset");
+
+let submit = document.createElement("input");
+submit.setAttribute("type", "submit");
+submit.classList.add("submit");
+header.append(submit);
+submit.disabled = true;
+// submit.style.display = "none"
+//////////////////////////////////////////////////////////////////////////////
+let mainDiv = document.getElementById("mainDiv");
+let table = document.createElement("table");
+
+input.addEventListener("input", () => {
+  if (input.value.length > 0) {
+    submit.disabled = false;
+    submit.style.display = "block"
+    submit.addEventListener("click", () => {
+      if (input.value.length > 0 || input.value !== "") {
+        let tr = document.createElement("tr");
+        let td = document.createElement("td");
+        let remove = document.createElement("button");
+        let edit = document.createElement("button");
+
+        remove.innerText = "Remove";
+        edit.innerText = "Edit";
+
+        remove.classList.toggle("remove");
+        edit.classList.toggle("edit");
+        edit.classList.toggle("accept");
+
+        td.innerText = input.value;
+        tr.append(td, remove, edit);
+        table.append(tr);
+        mainDiv.append(table);
+        input.value = "";
+        submit.disabled = true;
+        submit.style.display = "none"
+
+        remove.addEventListener("click", () => {
+          remove.parentElement.remove();
+        });
+
+        edit.addEventListener("click", () => {
+          edit.innerText = "Accept";
+          td.setAttribute("contenteditable", "true");
+
+          edit.addEventListener("click", () => {
+            td.setAttribute("contenteditable", "false");
+            edit.innerText = "Edit";
+          });
+        });
+      }
+    });
+  } else {
+    submit.disabled = true;
+    submit.style.display = "none"
+  }
+});
